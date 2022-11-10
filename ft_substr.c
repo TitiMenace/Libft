b0c7feb26872fc6492f490608cb97155ee435a94
TitiMenace <kreenkld@gmail.com>
2022-11-10 04:02:59 +0100

    avant le testeur

--- ft_substr.c
+++ ft_substr.c
@@ -1,46 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tschecro <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 05:18:52 by tschecro          #+#    #+#             */
-/*   Updated: 2022/11/09 09:49:06 by tschecro         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
+
+static int	getlen(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	count;
+	
+	count = 1;
+	while(s[start] && count <= len)
+	{
+			count++;
+			start++;
+	}
+			return (count);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	int				i;
-	unsigned long	j;
-	char			*out;
-	int				size;
+	char			*s2;
+	unsigned int	i;
+	unsigned int	count;
 
-	j = 0;
-	i = start;
-	if (len <= ft_strlen(s) - (int)start)
-		size = len;
-	else
-		size = ft_strlen(s) - (size_t)start;
-	if ((size_t)start >= ft_strlen(s))
-		size = 0;
-	out = malloc(sizeof(char) * (size + 1));
-	if (!out)
+	if (!s)
 		return (NULL);
-	while (s[i] && j < (unsigned long)size)
+	count = getlen(s, start, len);
+	s2 = malloc(sizeof(char) * count);
+	if (!s2)
+		return (NULL);
+	i = 0;
+	while(s[start] && i < len)
 	{
-		out[j] = s[i];
+		s2[i] = s[start];
 		i++;
-		j++;
+		start++;
 	}
-	out[j] = 0;
-	return (out);
-}
-
-int	main()
-{
-	printf("%s", ft_substr("yolesmecs", 3, 4 ));
+	s2[i] = 0;
+	return (s2);
 }
